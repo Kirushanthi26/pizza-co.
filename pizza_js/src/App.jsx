9a68d084d9f36e659c1c5de3e14dc8b7b865ec65
Kirushanthi26 <kirushanthikiru26@gmail.com>
2024-05-18 14:01:23 +0530

    style

--- pizza_js/src/App.jsx
+++ pizza_js/src/App.jsx
@@ -4,7 +4,7 @@ import Menu from "./Menu";
 
 const App = () => {
   return (
-    <div>
+    <div className=" font-mono text-gray-900 font-normal bg-orange-100 border-b-8 border-b-orange-300 min-h-[100vh] p-[3.2rem] pb-[6rem]">
       <Header />
       <Menu />
       <Footer />
--- pizza_js/src/Header.jsx
+++ pizza_js/src/Header.jsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
     <header>
-      <h1>Fast React Pizza Co.</h1>
+      <h1 className="text-yellow-500 uppercase text-center text-6xl font-light tracking-wider">
+        Fast React Pizza Co.
+      </h1>
     </header>
   );
 };
