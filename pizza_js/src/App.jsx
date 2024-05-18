6ca438072091cc4b4dfde8266fb52a93b0320f97
Kirushanthi26 <kirushanthikiru26@gmail.com>
2024-05-18 13:16:00 +0530

    header and other files

--- pizza_js/src/App.jsx
+++ pizza_js/src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const App = () => {
-  return <h1 className="text-3xl font-bold ">Hello world!</h1>;
+  return (
+    <div>
+      header
+      <br />
+      menu - pizza
+      <br />
+      footer
+    </div>
+  );
 };
 
 export default App;
